87dc3a90c0b4a17d4ba5b6d1987777cabd87bd62
mithunkumar123x <mithunkumar123x@gmail.com>
2023-11-04 21:18:58 +0530

    link api and learn jquery basics to call api

---
+++ script.js
@@ -0,0 +1,34 @@
+const colorSelect= document.getElementById('colorSelect');
+const body = document.querySelector("body")
+
+// buttons.forEach(function(button) {
+//   //console.log(button);  
+//   button.addEventListener('click' , function(e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === 'grey'){
+//         body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'white'){
+//         body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'blue'){
+//         body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'yellow'){
+//         body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'purple'){
+//         body.style.backgroundColor = e.target.id;
+
+//     }
+//     if(e.target.id === 'green'){
+//         body.style.background = 'green'
+//     }
+//   })  
+// })
+
+
+    colorSelect.addEventListener('change' , function (e) {
+        body.style.backgroundColor = e.target.value ;
+    });
